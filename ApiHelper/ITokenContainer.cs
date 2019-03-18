e4e28db7c4f1ee9d7ec9ef164b64ce2dc1385883
antzjatmika <30500524+antzjatmika@users.noreply.github.com>
2019-03-18 13:13:34 +0700

    Patch Pertama Helper
    Helper

---
+++ ApiHelper/ITokenContainer.cs
@@ -0,0 +1,23 @@
+﻿using ApiHelper.Models;
+namespace ApiHelper
+{
+    public interface ITokenContainer
+    {
+        //token untuk header validasi
+        object ApiToken { get; set; }
+        //role apa saja yang diassign ke user
+        object TokenRole { get; set; }
+        //role pertama yang dimiliki user (saat ini masih berlaku satu role untuk tiap user)
+        object RoleName { get; set; }
+        //id rekanan /id kar/id pcp/id portfolio/id adminsistem
+        object UserId { get; set; }
+        object SupervisorId { get; set; }
+        object IdRekananContact { get; set; }
+        object IdNotaris { get; set; }
+        object IdOrganisasi { get; set; }
+        object UserName { get; set; }
+        object UserEmail { get; set; }
+        object IdTypeOfRekanan { get; set; }
+        //object XLSPointer { get; set; }
+    }
+}
---
+++ ApiHelper/Response/ErrorStateResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace ApiHelper.Response
+{
+    using System.Collections.Generic;
+
+    public class ErrorStateResponse
+    {
+        public string Message { get; set; }
+        public IDictionary<string, string[]> ModelState { get; set; }
+    }
+}
