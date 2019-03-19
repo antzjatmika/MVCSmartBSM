b4d650610426665a32c9736b2a6545913e9e222b
antzjatmika <30500524+antzjatmika@users.noreply.github.com>
2019-03-19 17:53:33 +0700

    perbaikan datamodel

--- MVCSmartAPI01/Models/DB_SMART1.Designer.cs
+++ MVCSmartAPI01/Models/DB_SMART1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Project\SMART_BSM\Code\MVCSmart01\MVCSmartAPI01\Models\DB_SMART1.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Project\SMART_BSM\Code\MVCSmart01\MVCSmartBSM\MVCSmartAPI01\Models\DB_SMART1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
